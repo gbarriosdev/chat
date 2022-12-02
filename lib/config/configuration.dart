bab6d99bdb2b7f3773c473bd4819752dd2c108e8
gbarriosdev <gbarios@uninorte.edu.co>
2022-12-01 21:55:59 -0500

    version final

---
+++ lib/config/configuration.dart
@@ -0,0 +1,15 @@
+class Configuration {
+  static const apiKey =
+      "AIzaSyBv_yPA8MEWIBC2fSzmbTLpICpgq2cNndA"; // tomado de current_key
+  static const authDomain =
+      "reto3chat-f06da.firebaseapp.com"; // tomado de oauth_client
+  static const databaseURL =
+      "https://reto3chat-f06da-default-rtdb.firebaseio.com"; // tomado de firebase_url
+  static const projectId = "reto3chat-f06da"; // tomado de project_id
+  static const storageBucket =
+      "reto3chat-f06da.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = "335027608545"; //no lo vamos a usar
+  static const appId =
+      "1:335027608545:web:fc340469bf7cd295a090a0"; // tomado de mobilesdk_app_id
+  static const measurementId = "G-XBW7HTTT2M"; //no lo vamos a usar
+}
